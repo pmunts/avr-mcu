3b9698514ab5b84ce789e07ffbe8e5408f0692c6
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2006-07-12 16:09:08 +0000

    Cosmetic change to whitespace
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@662 06ee2f96-5407-11de-8bab-f3d7589a4122

--- conio.h
+++ conio.h
@@ -1,6 +1,7 @@
 /* Simple serial console services */
 
-// $Id: conio.h,v 1.1 2006-07-12 00:02:06 cvs Exp $
+// $Id: conio.h,v 1.2 2006-07-12 16:09:08 cvs Exp $
+
+void conio_init(unsigned long int baudrate);
 
 void cputs(char *s);
-void conio_init(unsigned long int baudrate);
