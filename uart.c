24ffd35f4ad60625348b014c6bf69b6e071b93c1
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2006-08-17 19:32:54 +0000

    Cosmetic changes to allow compiling for ATmega168
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@677 06ee2f96-5407-11de-8bab-f3d7589a4122

--- uart.c
+++ uart.c
@@ -1,6 +1,6 @@
 /* Basic UART polling serial console driver */
 
-// $Id: uart.c,v 1.2 2006-07-12 17:17:11 cvs Exp $
+// $Id: uart.c,v 1.3 2006-08-17 19:32:54 cvs Exp $
 
 #include <avr/io.h>
 #include <stdio.h>
@@ -19,20 +19,20 @@ void uart_init(unsigned long int baudrate)
   UBRR0H = b / 256;
   UBRR0L = b % 256;
   UCSR0A = 0x00;
-  UCSR0B = _BV(TXEN) | _BV(RXEN);
-  UCSR0C = _BV(UCSZ1) | _BV(UCSZ0);
+  UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+  UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
 }
 
 int uart_putch(char c, FILE *f)
 {
   if (c == '\n') uart_putch('\r', f);
-  loop_until_bit_is_set(UCSR0A, UDRE);
+  loop_until_bit_is_set(UCSR0A, UDRE0);
   UDR0 = c;
   return 0;
 }
 
 int uart_getch(FILE *f)
 {
-  loop_until_bit_is_set(UCSR0A, RXC);
+  loop_until_bit_is_set(UCSR0A, RXC0);
   return UDR0;
 }
