d23535bc075c59b217cc232332838f28c02e7492
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2006-10-06 14:42:54 +0000

    Added convenience macros for UDR0 etc. for microcontrollers such as the ATmega8535 that only have a single serial port.
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@693 06ee2f96-5407-11de-8bab-f3d7589a4122

--- uart.c
+++ uart.c
@@ -1,11 +1,61 @@
 /* Basic UART polling serial console driver */
 
-// $Id: uart.c,v 1.3 2006-08-17 19:32:54 cvs Exp $
+// $Id: uart.c,v 1.4 2006-10-06 14:42:54 cvs Exp $
 
 #include <avr/io.h>
 #include <stdio.h>
 
 #include "uart.h"
+
+// Define compatibility macros
+
+#ifndef UDR0
+#define UDR0	UDR
+#endif
+
+#ifndef UBRR0H
+#define UBRR0H	UBRRH
+#endif
+
+#ifndef UBRR0L
+#define UBRR0L	UBRRL
+#endif
+
+#ifndef UCSR0A
+#define UCSR0A	UCSRA
+#endif
+
+#ifndef UCSR0B
+#define UCSR0B	UCSRB
+#endif
+
+#ifndef UCSR0C
+#define UCSR0C	UCSRC
+#endif
+
+#ifndef UCSZ00
+#define UCSZ00	UCSZ0
+#endif
+
+#ifndef UCSZ01
+#define UCSZ01	UCSZ1
+#endif
+
+#ifndef UDRE0
+#define UDRE0	UDRE
+#endif
+
+#ifndef RXC0
+#define RXC0	RXC
+#endif
+
+#ifndef RXEN0
+#define RXEN0	RXEN
+#endif
+
+#ifndef TXEN0
+#define TXEN0	TXEN
+#endif
 
 unsigned long int CPUFREQ = 16000000;
 
