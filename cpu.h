403233cd9924ad623b89d162a4331d025f9f1e82
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-09-22 11:47:17 +0000

    Added: #include <avr/interrupt.h> #include <avr/pgmspace.h> #include "adc.h" #include "conio.h"
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@3038 06ee2f96-5407-11de-8bab-f3d7589a4122

--- cpu.h
+++ cpu.h
@@ -6,7 +6,12 @@
 #define F_CPU 16000000L
 #endif
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 #include <avr/wdt.h>
 
 #include <util/delay.h>
+
+#include "adc.h"
+#include "conio.h"
