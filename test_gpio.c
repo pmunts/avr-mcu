fbd55741b947936c9682fcb927c94533eb27b720
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-09-22 11:39:08 +0000

    Only write to GPIO ports that are defined
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@3035 06ee2f96-5407-11de-8bab-f3d7589a4122

--- test_gpio.c
+++ test_gpio.c
@@ -10,29 +10,49 @@ int main(void)
 
   wdt_disable();
 
-#ifndef __AVR_ATmega168__
-//  DDRA = 0xFF;
+#ifdef DDRA
+  DDRA = 0xFF;
 #endif
+#ifdef DDRB
   DDRB = 0xFF;
-//  DDRC = 0xFF;
- // DDRD = 0xFF;
-#ifdef __AVR_ATmega128__
+#endif
+#ifdef DDRC
+  DDRC = 0xFF;
+#endif
+#ifdef DDRD
+  DDRD = 0xFF;
+#endif
+#ifdef DDRE
   DDRE = 0xFF;
+#endif
+#ifdef DDRF
   DDRF = 0xFF;
+#endif
+#ifdef DDRG
   DDRG = 0xFF;
 #endif
 
   for (a = 0;; a++)
   {
-#ifndef __AVR_ATmega168__
-//    PORTA = a;
+#ifdef PORTA
+    PORTA = a;
 #endif
+#ifdef PORTB
     PORTB = a;
-//    PORTC = a;
-//    PORTD = a;
-#ifdef __AVR_ATmega128__
+#endif
+#ifdef PORTC
+    PORTC = a;
+#endif
+#ifdef PORTD
+    PORTD = a;
+#endif
+#ifdef PORTE
     PORTE = a;
+#endif
+#ifdef PORTF
     PORTF = a;
+#endif
+#ifdef PORTG
     PORTG = a;
 #endif
   }
