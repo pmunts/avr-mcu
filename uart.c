792de847017ce0eeea3d2d8577f9345115363f6c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-09-30 08:39:08 +0000

    Cleaned up the UART register macros to support almost any device on the first or second U(S)ART.
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@3081 06ee2f96-5407-11de-8bab-f3d7589a4122

--- uart.c
+++ uart.c
@@ -7,152 +7,256 @@
 
 #include <avr/interrupt.h>
 
-// The following parts only have USART1
-
-#ifdef __AVR_AT90USB1286__
-#define CONFIG_UART1
-#endif
-
-// Catch-all: default to U(S)ART0
-
-#if !defined(UDR) && !defined(CONFIG_UART0) && !defined(CONFIG_UART1)
-#define CONFIG_UART0
-#endif
-
 // The following hodgepodge of macros are an attempt to make sense of the
 // myriad variations of serial ports in the AVR family.
 
-// Define compatibility macros
+// Serial console is on FIRST serial port
 
-#if defined(CONFIG_UART0) && defined(UDR0)
-#define UDR UDR0
-#ifndef UBRRH
-#define UBRRH UBRR0H
+#if defined(CONFIG_CONSOLE_UART0) || (!defined(CONFIG_CONSOLE_UART0) && !defined(CONFIG_CONSOLE_UART1))
+
+#if defined(UDR)
+#define zzUDR		UDR
+#elif defined(UDR0)
+#define zzUDR		UDR0
+#elif defined(UDR1)
+#define zzUDR		UDR1
 #endif
-#ifdef UBRR0
-#define UBRRL UBRR0
+
+#if defined(UBRRH)
+#define zzUBRRH		UBRRH
+#elif defined(UBRR0H)
+#define zzUBRRH		UBRR0H
+#elif defined(UBRR1H)
+#define zzUBRRH		UBRR1H
+#endif
+
+#if defined(UBRRL)
+#define zzUBRRL		UBRRL
+#elif defined(UBRR0)
+#define zzUBRRL		UBRR0
+#elif defined(UBRR0L)
+#define zzUBRRL		UBRR0L
+#elif defined(UBRR1)
+#define zzUBRRL		UBRR1
+#elif defined(UBRR1L)
+#define zzUBRRL		UBRR1L
+#endif
+
+#if defined(UCSRA)
+#define zzUCSRA		UCSRA
+#elif defined(UCSR0A)
+#define zzUCSRA		UCSR0A
+#elif defined(UCSR1A)
+#define zzUCSRA		UCSR1A
+#endif
+
+#if defined(UCSRB)
+#define zzUCSRB		UCSRB
+#elif defined(UCSR0B)
+#define zzUCSRB		UCSR0B
+#elif defined(UCSR1B)
+#define zzUCSRB		UCSR1B
+#endif
+
+#if defined(UCSRC)
+#define zzUCSRC		UCSRC
+#elif defined(UCSR0C)
+#define zzUCSRC		UCSR0C
+#elif defined(UCSR1C)
+#define zzUCSRC		UCSR1C
+#endif
+
+#if defined(U2X)
+#define zzU2X		U2X
+#elif defined(U2X0)
+#define zzU2X		U2X0
+#elif defined(U2X1)
+#define zzU2X		U2X1
+#endif
+
+#if defined(RXEN)
+#define zzRXEN		RXEN
+#elif defined(RXEN0)
+#define zzRXEN		RXEN0
+#elif defined(RXEN1)
+#define zzRXEN		RXEN1
+#endif
+
+#if defined(TXEN)
+#define zzTXEN		TXEN
+#elif defined(TXEN0)
+#define zzTXEN		TXEN0
+#elif defined(TXEN1)
+#define zzTXEN		TXEN1
+#endif
+
+#if defined(RXCIE)
+#define zzRXCIE		RXCIE
+#elif defined(RXCIE0)
+#define zzRXCIE		RXCIE0
+#elif defined(RXCIE1)
+#define zzRXCIE		RXCIE1
+#endif
+
+#if defined(UDRE)
+#define zzUDRE		UDRE
+#elif defined(UDRE0)
+#define zzUDRE		UDRE0
+#elif defined(UDRE1)
+#define zzUDRE		UDRE1
+#endif
+
+#if defined(UCSZ0)
+#define zzUCSZ0		UCSZ0
+#elif defined(UCSZ00)
+#define zzUCSZ0		UCSZ00
+#elif defined(UCSZ10)
+#define zzUCSZ0		UCSZ10
+#endif
+
+#if defined(UCSZ1)
+#define zzUCSZ1		UCSZ1
+#elif defined(UCSZ01)
+#define zzUCSZ1		UCSZ01
+#elif defined(UCSZ11)
+#define zzUCSZ1		UCSZ11
+#endif
+
+#if defined(URSEL)
+#define zzURSEL		URSEL
+#elif defined(URSEL0)
+#define zzURSEL		URSEL0
+#elif defined(URSEL1)
+#define zzURSEL		URSEL1
+#endif
+
+#if defined(UART_RX_vect)
+#define zzUART_RX_vect	UART_RX_vect
+#elif defined(UART0_RX_vect)
+#define zzUART_RX_vect	UART0_RX_vect
+#elif defined(USART_RX_vect)
+#define zzUART_RX_vect	USART_RX_vect
+#elif defined(USART_RXC_vect)
+#define zzUART_RX_vect	USART_RXC_vect
+#elif defined(USARTRXC_vect)
+#define zzUART_RX_vect	USARTRXC_vect
+#elif defined(USART0_RX_vect)
+#define zzUART_RX_vect	USART0_RX_vect
+#elif defined(USART0_RXC_vect)
+#define zzUART_RX_vect	USART0_RXC_vect
+#elif defined(UART1_RX_vect)
+#define zzUART_RX_vect	UART1_RX_vect
+#elif defined(USART1_RX_vect)
+#define zzUART_RX_vect	USART1_RX_vect
+#elif defined(USART1_RXC_vect)
+#define zzUART_RX_vect	USART1_RXC_vect
+#endif
+
+#endif
+
+// Serial console is on SECOND serial port
+
+#if defined(CONFIG_CONSOLE_UART1)
+
+#if defined(UDR1)
+#define zzUDR		UDR1
 #else
-#define UBRRL UBRR0L
-#endif
-#define UCSRA UCSR0A
-#define UCSRB UCSR0B
-#ifdef UCSR0C
-#define UCSRC UCSR0C
-#endif
-#ifndef U2X
-#define U2X U2X0
-#endif
-#ifndef RXEN
-#define RXEN RXEN0
-#endif
-#ifndef TXEN
-#define TXEN TXEN0
-#endif
-#ifndef RXCIE
-#define RXCIE RXCIE0
-#endif
-#ifndef UDRE
-#define UDRE UDRE0
-#endif
-#ifndef UCSZ0
-#define UCSZ0 UCSZ00
-#endif
-#ifndef UCSZ1
-#define UCSZ1 UCSZ01
-#endif
-#ifdef URSEL0
-#define URSEL URSEL0
-#endif
-#ifdef UART0_RX_vect
-#define USART_RX_vect UART0_RX_vect
-#endif
-#ifdef USART0_RX_vect
-#define USART_RX_vect USART0_RX_vect
-#endif
-#ifdef USART0_RXC_vect
-#define USART_RX_vect USART0_RXC_vect
-#endif
+#error Cannot find UDR
 #endif
 
-#if defined(CONFIG_UART1) && defined(UDR1)
-#define UDR UDR1
-#ifndef UBRRH
-#define UBRRH UBRR1H
-#endif
-#ifdef UBRR1
-#define UBRRL UBRR1
-#else
-#define UBRRL UBRR1L
-#endif
-#define UCSRA UCSR1A
-#define UCSRB UCSR1B
-#ifdef UCSR1C
-#define UCSRC UCSR1C
-#endif
-#ifndef U2X
-#define U2X U2X1
-#endif
-#ifndef RXEN
-#define RXEN RXEN1
-#endif
-#ifndef TXEN
-#define TXEN TXEN1
-#endif
-#ifndef RXCIE
-#define RXCIE RXCIE1
-#endif
-#ifndef UDRE
-#define UDRE UDRE1
-#endif
-#ifndef UCSZ0
-#define UCSZ0 UCSZ10
-#endif
-#ifndef UCSZ1
-#define UCSZ1 UCSZ11
-#endif
-#ifdef URSEL1
-#define URSEL URSEL1
-#endif
-#ifdef UART1_RX_vect
-#define USART_RX_vect UART1_RX_vect
-#endif
-#ifdef USART1_RX_vect
-#define USART_RX_vect USART1_RX_vect
-#endif
-#ifdef USART1_RXC_vect
-#define USART_RX_vect USART1_RXC_vect
-#endif
+#if defined(UBRR1H)
+#define zzUBRRH		UBRR1H
+#elif defined(UBRRH)
+#define zzUBRRH		UBRRH
 #endif
 
-// For older parts with UART instead of USART
-
-#if !defined(USART_RX_vect) && defined(UART_RX_vect)
-#define USART_RX_vect UART_RX_vect
+#if defined(UBRR1)
+#define zzUBRRL		UBRR1
+#elif defined(UBRR1L)
+#define zzUBRRL		UBRR1L
 #endif
 
-// For ATmega32 and possibly others
-
-#if !defined(USART_RX_vect) && defined(USART_RXC_vect)
-#define USART_RX_vect USART_RXC_vect
+#if defined(UCSR1A)
+#define zzUCSRA		UCSR1A
 #endif
 
-#ifdef UDR
+#if defined(UCSR1B)
+#define zzUCSRB		UCSR1B
+#endif
+
+#if defined(UCSR1C)
+#define zzUCSRC		UCSR1C
+#endif
+
+#if defined(U2X1)
+#define zzU2X		U2X1
+#elif defined(U2X)
+#define zzU2X		U2X
+#endif
+
+#if defined(RXEN1)
+#define zzRXEN		RXEN1
+#elif defined(RXEN)
+#define zzRXEN		RXEN
+#endif
+
+#if defined(TXEN1)
+#define zzTXEN		TXEN1
+#elif defined(TXEN)
+#define zzTXEN		TXEN
+#endif
+
+#if defined(RXCIE1)
+#define zzRXCIE		RXCIE1
+#elif defined (RXCIE)
+#define zzRXCIE		RXCIE
+#endif
+
+#if defined(UDRE1)
+#define zzUDRE		UDRE1
+#elif defined(UDRE)
+#define zzUDRE		UDRE
+#endif
+
+#if defined(UCSZ10)
+#define zzUCSZ0		UCSZ10
+#endif
+
+#if defined(UCSZ11)
+#define zzUCSZ1		UCSZ11
+#endif
+
+#if defined(URSEL1)
+#define zzURSEL		URSEL1
+#endif
+
+#if defined(UART1_RX_vect)
+#define zzUART_RX_vect	UART1_RX_vect
+#elif defined(USART1_RX_vect)
+#define zzUART_RX_vect	USART1_RX_vect
+#elif defined(USART1_RXC_vect)
+#define zzUART_RX_vect	USART1_RXC_vect
+#endif
+
+#endif
+
+#ifdef zzUDR
 void uart_init(unsigned long int baudrate)
 {
   unsigned int b;
 
-  UDR = 0;
+  zzUDR = 0;
 
   b = F_CPU/8/baudrate - 1;
-  UBRRH = b / 256;
-  UBRRL = b % 256;
-  UCSRA = _BV(U2X);
-  UCSRB = _BV(TXEN) | _BV(RXEN) | _BV(RXCIE);
-#ifdef UCSRC
-#ifdef URSEL
-  UCSRC = _BV(URSEL) | _BV(UCSZ1) | _BV(UCSZ0);
+  zzUBRRH = b / 256;
+  zzUBRRL = b % 256;
+  zzUCSRA = _BV(zzU2X);
+  zzUCSRB = _BV(zzTXEN) | _BV(zzRXEN) | _BV(zzRXCIE);
+#ifdef zzUCSRC
+#ifdef zzURSEL
+  zzUCSRC = _BV(zzURSEL) | _BV(zzUCSZ1) | _BV(zzUCSZ0);
 #else
-  UCSRC = _BV(UCSZ1) | _BV(UCSZ0);
+  zzUCSRC = _BV(zzUCSZ1) | _BV(zzUCSZ0);
 #endif
 #endif
 
@@ -161,10 +265,10 @@ void uart_init(unsigned long int baudrate)
 
 int uart_putch(char c)
 {
-  while (bit_is_clear(UCSRA, UDRE))
+  while (bit_is_clear(zzUCSRA, zzUDRE))
     wdt_reset();
 
-  UDR = c;
+  zzUDR = c;
   return 0;
 }
 
@@ -179,11 +283,11 @@ volatile unsigned char UART_Rcv_buf[RXBUFSIZE];
 
 /* Serial port receive interrupt service routine */
 
-ISR(USART_RX_vect)
+ISR(zzUART_RX_vect)
 {
   unsigned char c;
 
-  c = UDR;
+  c = zzUDR;
 
   if (UART_Rcv_count < RXBUFSIZE)
   {
