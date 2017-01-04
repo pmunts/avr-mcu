7c715ad7d5e0388c0d2bbce9aba2f50d4b7e6929
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2017-01-04 08:27:14 +0000

    Upgraded copyright message for 2017
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@11696 06ee2f96-5407-11de-8bab-f3d7589a4122

--- common/common.mk
+++ common/common.mk
@@ -1,6 +1,6 @@
 # Common library make definitions
 
-# Copyright (C)2013-2016, Philip Munts, President, Munts AM Corp.
+# Copyright (C)2013-2017, Philip Munts, President, Munts AM Corp.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
