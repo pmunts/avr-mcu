2b85fe14204b7a22c2a709105c671dbe5344a38e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-01-11 08:49:51 +0000

    Updated the copyright year for 2018
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@14036 06ee2f96-5407-11de-8bab-f3d7589a4122

--- gcc/include/legorc.h
+++ gcc/include/legorc.h
@@ -2,7 +2,7 @@
 
 // Reference the specification "LEGO(R) Power Functions RC Version 1.20"
 
-// Copyright (C)2017, Philip Munts, President, Munts AM Corp.
+// Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
