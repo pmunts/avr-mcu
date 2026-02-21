2c91d99ac178bba44c8b44040d4f91b984cf6698
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-21 23:52:50 +0000

    Added Arduino AVR Servo Test
    git-svn-id: svn+ssh://localhost/src/trunk/AVR@24506 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ arduino/test_servo/test_servo.ino
@@ -0,0 +1,54 @@
+// Arduino AVR Servo Test
+
+// Copyright (C)2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Hardware configuration (bottom to top):
+//
+// Arduino Uno R3 (5V logic!)
+
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo outp;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Arduino AVR Servo Test");
+  Serial.setTimeout(10000);
+
+  outp.attach(9, 1000, 2000);
+}
+
+void loop()
+{
+  for (int angle = 0; angle <= 180; angle++)
+  {
+    outp.write(angle);
+    delay(15);
+  }
+
+  for (int angle = 180; angle >= 0; angle--)
+  {
+    outp.write(angle);
+    delay(15);
+  }
+}
